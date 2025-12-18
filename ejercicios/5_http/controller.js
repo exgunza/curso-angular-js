4a7af0e72259398aad31648c4d91d00a628c25f5
Estalin Gunza <exgunza@yahoo.com>
2025-12-18 13:05:52 -0500

    EGU-feat; Uso de http

---
+++ ejercicios/5_http/controller.js
@@ -0,0 +1,27 @@
+angular.module("MyFirstApp", [])
+    .controller("FirstController", function($scope,$http){
+        $scope.posts = [];
+        $scope.newPost = {};
+        $http.get("https://jsonplaceholder.typicode.com/posts")
+            .success(function(data){
+                console.log(data);
+                $scope.posts = data;
+            })
+            .error(function(err){
+                console.log(err);
+            });
+        $scope.addPost = function(){
+            $http.post("https://jsonplaceholder.typicode.com/posts",{
+                title: $scope.newPost.title,
+                body: $scope.newPost.body,
+                userId: 1
+            })
+            .success(function(data, status, headers, config){
+                $scope.posts.push(data);
+                $scope.newPost = {};
+            })
+            .error(function(error, status, headers, config){
+                console.log(error);
+            });
+        }
+    });
